42b795d5dc30817a107096684ca3bf1618a0ac33
millatur <minnatur7373@gmail.com>
2024-06-04 20:17:40 +0600

    ahdiuyh

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     // function to create user
     public function createUser(Request $request){
+
+        $user = user::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+        ]);
 
         return response()->json(['atatus' => true, 'message' => "Hello World", 'data' => $request->all()]);
 
