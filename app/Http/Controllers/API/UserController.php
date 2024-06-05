f66c7d843f36975e03946005e1b0b851c01587b7
millatur <minnatur7373@gmail.com>
2024-06-05 06:35:12 +0600

    asdiuh

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,11 +5,25 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     // function to create user
     public function createUser(Request $request){
+
+        // validation
+        $validator = Validator::make($request->all(),[
+            'name' => "required|string",
+            'email' => "required|string",
+            'phone' => "required|numeric",
+            'password' => "required|min:6"
+        ]);
+
+        if($validator->fails()){
+            $result = array('status' => false, 'message' => "Validation Error Occured", 'error_message' => $validator->error());
+            return response()->json($result,$responseCode);
+        }
 
         $user = user::create([
             'name' => $request->name,
